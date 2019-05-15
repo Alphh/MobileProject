c5a616a6589f636b3a2d9c2e2f5e642f99fdc060
Keith <keithvanhear@gmail.com>
2019-05-16 00:51:58 +0200

    - Added view wallpaper - Added set as background function - also added base wallpaper description - general UI enhancement

--- app/src/main/java/com/example/mobileproject/Common/Common.java
+++ app/src/main/java/com/example/mobileproject/Common/Common.java
@@ -1,9 +1,13 @@
 package com.example.mobileproject.Common;
 
+import com.example.mobileproject.CategoryModel.Wallpaperitem;
+
 public class Common {
 
     public static final String STR_CATEGORY_BACKGROUND = "CategoryBackground";
     public static final String STR_WALLPAPER = "Wallpapers";
     public static String CATEGORY_SELECTED;
     public static String CATEGORY_ID_SELECTED;
+
+    public static Wallpaperitem select_background = new Wallpaperitem();
 }
--- app/src/main/java/com/example/mobileproject/ListWallpaper.java
+++ app/src/main/java/com/example/mobileproject/ListWallpaper.java
@@ -1,6 +1,7 @@
 package com.example.mobileproject;
 
 import android.app.DownloadManager;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -102,7 +103,11 @@ public class ListWallpaper extends AppCompatActivity
                     @Override
                     public void onClick(View view,int position)
                     {
-                            //later use
+
+                        Intent intent = new Intent(ListWallpaper.this,ViewWallpaper.class);
+                        Common.select_background = model;
+                        startActivity(intent);
+
                     }
                 });
 
