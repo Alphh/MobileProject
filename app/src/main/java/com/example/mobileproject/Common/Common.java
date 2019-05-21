b05d2489e0be4abf1dc71d2e2708257b69b1d686
Keith <keithvanhear@gmail.com>
2019-05-21 19:18:23 +0200

    - Added upload Wallpaper - Removed Explore and about buttons

--- app/src/main/java/com/example/mobileproject/Common/Common.java
+++ app/src/main/java/com/example/mobileproject/Common/Common.java
@@ -8,6 +8,7 @@ public class Common {
     public static final String STR_WALLPAPER = "Wallpapers";
     public static String CATEGORY_SELECTED;
     public static String CATEGORY_ID_SELECTED;
+    public static final int PICK_IMAGE_REQUEST = 1002;
 
     public static Wallpaperitem select_background = new Wallpaperitem();
 }
--- app/src/main/java/com/example/mobileproject/HomeActivity.java
+++ app/src/main/java/com/example/mobileproject/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -24,6 +27,7 @@ public class HomeActivity extends AppCompatActivity
     ViewPager viewPager;
     TabLayout tabLayout;
 
+    BottomNavigationView menu_bottom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,18 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Wallpaper");
         setSupportActionBar(toolbar);
+
+        menu_bottom = (BottomNavigationView) findViewById(R.id.navigation);
+        menu_bottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.action_upload)
+                    startActivity(new Intent(HomeActivity.this,UploadWallpaper.class));
+                return false;
+            }
+        });
+
+
 
         viewPager = (ViewPager)findViewById(R.id.viewPager);
         MyFragmentAdapter adapter = new MyFragmentAdapter(getSupportFragmentManager(), this);
