809f8c603ae6d903fee0f8000dd765c41e077a0c
Luka <djincharadzeluka@yahoo.com>
2019-05-24 23:19:02 +0200

    24.05.2019

--- app/src/main/java/com/example/mobileproject/Fragment/AllFragment.java
+++ app/src/main/java/com/example/mobileproject/Fragment/AllFragment.java
@@ -1,41 +1,161 @@
 package com.example.mobileproject.Fragment;
 
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mobileproject.CategoryModel.Wallpaperitem;
+import com.example.mobileproject.Common.Common;
+import com.example.mobileproject.Interface.itemClickListener;
 import com.example.mobileproject.R;
+import com.example.mobileproject.ViewHolder.ListWallpaperViewHolder;
+import com.example.mobileproject.ViewWallpaper;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class AllFragment extends Fragment {
+    FirebaseDatabase database;
+    Query query;
 
+    FirebaseRecyclerOptions<Wallpaperitem> options;
+    FirebaseRecyclerAdapter<Wallpaperitem, ListWallpaperViewHolder> adapter;
+
+    RecyclerView recyclerView;
 
     private static AllFragment INSTANCE=null;
 
-
     public AllFragment() {
-        // Required empty public constructor
+        database = FirebaseDatabase.getInstance();
+        query = FirebaseDatabase.getInstance().getReference(Common.STR_WALLPAPER);
+
+        options = new FirebaseRecyclerOptions.Builder<Wallpaperitem>()
+                .setQuery(query, Wallpaperitem.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Wallpaperitem, ListWallpaperViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull final ListWallpaperViewHolder holder, int position, @NonNull final Wallpaperitem model) {
+
+                Picasso.get()
+                        .load(model.getImageUrl())
+                        .into(holder.wallpaper, new Callback() {
+                            @Override
+                            public void onSuccess() {
+
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                Picasso.get()
+                                        .load(model.getImageUrl())
+                                        .error(R.drawable.ic_cloud_upload_black_24dp)
+                                        .into(holder.wallpaper, new Callback() {
+                                            @Override
+                                            public void onSuccess() {
+
+                                            }
+
+                                            @Override
+                                            public void onError(Exception e) {
+                                                Log.e("ERROR", "Could not fetch image");
+                                            }
+                                        });
+                            }
+                        });
+
+                holder.setItemClickListener(new itemClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        Intent intent = new Intent(getActivity(), ViewWallpaper.class);
+                        Common.select_background = model;
+                        startActivity(intent);
+
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ListWallpaperViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View itemView = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.layout_wallpaper_item, viewGroup, false);
+
+
+                return new ListWallpaperViewHolder(itemView);
+            }
+        };
+
     }
 
-    public static AllFragment getInstance()
-    {
+    public static AllFragment getInstance() {
         if(INSTANCE == null)
             INSTANCE = new AllFragment();
         return INSTANCE;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all, container, false);
+        View view = inflater.inflate(R.layout.activity_list_wallpaper, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_list_wallpaper);
+        recyclerView.setHasFixedSize(true);
+        Toolbar temp = view.findViewById(R.id.toolbar);
+        temp.setVisibility(View.GONE);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        setWallpaper();
+
+        return view;
     }
 
+    private void setWallpaper() {
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (adapter != null)
+            adapter.startListening();
+
+    }
+
+    @Override
+    public void onStop() {
+        if (adapter != null)
+            adapter.stopListening();
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (adapter != null)
+            adapter.startListening();
+
+    }
 }
--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -1,22 +1,11 @@
 package com.example.mobileproject;
 
-import android.app.WallpaperManager;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,24 +25,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     },1000); //sleep
-
-
-
-
-
-
-
-
-
-
-        findViewById(R.id.BT_images_main).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, DisplayImage.class);
-                startActivity(i);
-            }
-        });
-
-
     }
 }
